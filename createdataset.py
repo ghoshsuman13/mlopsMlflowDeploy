864f8811a797198e0dfccbcd1eafdc7c3d83ee65
suman <ghosh.suman49@gmail.com>
2025-01-09 23:26:43 +0530

    changing csv

--- createdataset.py
+++ createdataset.py
@@ -3,9 +3,9 @@ import pandas as pd
 from sklearn.datasets import make_regression
 
 # Generate synthetic regression data
-X, y = make_regression(n_samples=100, n_features=1, noise=0.1, random_state=42)
+X, y = make_regression(n_samples=100, n_features=1, noise=0.2, random_state=42)
 data = pd.DataFrame({'feature': X.flatten(), 'target': y})
 
 # Save dataset
-data.to_csv("./mlopsMlflowDeploy/data/dataset.csv", index=False)
+data.to_csv("data/dataset.csv", index=False)
 print("Dataset saved to data/dataset.csv")
